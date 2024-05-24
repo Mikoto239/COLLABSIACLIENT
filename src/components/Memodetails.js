a744b73b195a79cd991c002d31091ac2e0354d32
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-24 17:00:20 +0800

    Update Memodetails.js

--- src/components/Memodetails.js
+++ src/components/Memodetails.js
@@ -1,186 +1,113 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams, Link } from 'react-router-dom';
 import axios from 'axios';
-import HeaderDashboard from '../components/headerdashboard';
-import Footer from '../components/footer';
-import '../App';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import HeaderDashboard from '../components/HeaderDashboard';
+import Footer from '../components/Footer';
 import SidebarUser from '../../components/SidebarUser';
 import { pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`;
 
-
-
-const UserRecieveMemoDetails = ({ match }) => {
+const UserRecieveMemoDetails = () => {
   const [memoDetails, setMemoDetails] = useState(null);
-  const [isAcknowledged, setIsAcknowledged] = useState(false); // New state to track acknowledgment
+  const [isAcknowledged, setIsAcknowledged] = useState(false);
+  const [pdfUrl, setPdfUrl] = useState('');
   const history = useHistory();
   const { memoId } = useParams();
-  const [acknowledgeStatus, setAcknowledgeStatus] = useState(false);
-  const token = localStorage.getItem('token')
-  const [activeMenuItem, setActiveMenuItem] = useState('Memo');
-  const [pdfUrl, setPdfUrl] = useState('');
-
-  
- 
-
-
-  const handleMenuItemClick = (menuItem) => {
-    if (menuItem === 'Dashboard') {
-      history.push('/user/dashboard');
-    } else if  (menuItem === 'Memo Manager') {
-      history.push('/user/memo');
-    }
-   
-   else if  (menuItem === 'Report List') {
-      history.push('/user/report_list');
-    } else if  (menuItem === 'Calendar') {
-      history.push('/user/calendar');
-    }
-  };
-
+  const token = localStorage.getItem('token');
 
   useEffect(() => {
     const fetchMemoDetails = async () => {
       try {
+        const getMeResponse = await axios.get('https://collabsiaserver.onrender.com/api/getme', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
 
-        const getme = await axios.get('https://collabsiaserver.onrender.com/api/getme', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
+        const myEmail = getMeResponse.data.user.email;
+
+        const memoResponse = await axios.get(`https://collabsiaserver.onrender.com/api/memo/details/${memoId}?email=${myEmail}`);
+        setMemoDetails(memoResponse.data.memo);
+
+        const pdfResponse = await axios.get(`https://collabsiaserver.onrender.com/api/memo/pdfdetails/${memoId}?email=${myEmail}`, {
+          responseType: 'blob',
         });
-  
-        const myemail = getme.data.user.email;
-  
-        const response = await axios.get(`https://collabsiaserver.onrender.com/api/memo/details/${memoId}?email=${myemail}`);
-        setMemoDetails(response.data.memo);
-       
-        const res = await axios.get(`https://collabsiaserver.onrender.com/api/memo/pdfdetails/${memoId}?email=${myemail}`, {
-          responseType: 'blob', // Specify the response type as blob
-        });
-    
-        const pdfUrl = URL.createObjectURL(res.data);
+
+        const pdfUrl = URL.createObjectURL(pdfResponse.data);
         setPdfUrl(pdfUrl);
-        // Correct acknowledgment request
-        const acknowledgmentResponse = await axios.post(
-          `https://collabsiaserver.onrender.com/api/Iacknowledge/${memoId}`,
-          { email: myemail }
-        );
-  
+
+        const acknowledgmentResponse = await axios.post(`https://collabsiaserver.onrender.com/api/Iacknowledge/${memoId}`, { email: myEmail });
         setIsAcknowledged(acknowledgmentResponse.data.acknowledgeStatus);
-        setAcknowledgeStatus(acknowledgmentResponse.data.acknowledgeStatus);
       } catch (error) {
+        console.error('Error fetching memo details:', error);
         history.goBack();
       }
     };
-  
+
     fetchMemoDetails();
-  }, [memoId, token,history]);
-  
+  }, [memoId, token, history]);
 
   const handleAcknowledge = async () => {
-    let email;
-    let name;
-  
     try {
       const response = await axios.get('https://collabsiaserver.onrender.com/api/getme', {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-        },
+        headers: { Authorization: `Bearer ${token}` },
       });
-  
+
       if (response.status === 200 && response.data) {
-        email = response.data.user.email;
-        name = response.data.user.name;
-        
-  
-        const acknowledge = await axios.post(`https://collabsiaserver.onrender.com/api/memo/acknowledge/${memoId}`, { email,name});
-  
-        if (acknowledge.status === 200) {
+        const email = response.data.user.email;
+        const name = response.data.user.name;
+
+        const acknowledgeResponse = await axios.post(`https://collabsiaserver.onrender.com/api/memo/acknowledge/${memoId}`, { email, name });
+
+        if (acknowledgeResponse.status === 200) {
           setIsAcknowledged(true);
-        } else if (acknowledge.status === 404) {
-          console.error('Memo not found. Please check the memo ID.');
         } else {
-          console.error('Error acknowledging memo:', acknowledge.statusText);
+          console.error('Error acknowledging memo:', acknowledgeResponse.statusText);
         }
       } else {
-        console.error('Error fetching user details:' + email);
+        console.error('Error fetching user details');
       }
     } catch (error) {
       console.error('Error acknowledging memo:', error);
-  
-      if (error.response) {
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
-      }
     }
   };
-    
-  
-  
-  
 
-if (!memoDetails) {
-  return <p>Loading...</p>;
-}
-
+  if (!memoDetails) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
       <HeaderDashboard />
-      <div className="content-header">
-        <div>
-          <h1>Receive Memo</h1>
-        </div>
-        <div>
-          <input
-            className="search"
-            type="text"
-            placeholder="Search"
-          />
-        </div>
-      </div>
+      
 
       <div className="dashboard">
-       <SidebarUser/>
-        </div>
-
-        <div className='content'>
-     
+        <SidebarUser />
+        <div className="content">
           <div className="memo-details-container">
-          <Link to={'/user/memo'} className="link-to-send" > Back </Link>
+            <Link to="/user/memo" className="link-to-send">Back</Link>
             <h1>Memo Details</h1>
-          
             <p className="memo-title-details">Title: {memoDetails.title}</p>
             <p className="memo-sender-details">From: {memoDetails.sender}</p>
             <p className="memo-timestamp-details">Timestamp: {new Date(memoDetails.createdAt).toLocaleString()}</p>
             <p className="memo-content-details">Content: {memoDetails.content}</p>
             <div className="memo-pdf-container">
-                {pdfUrl ? (
-                  <iframe title="PDF Viewer" src={pdfUrl} width="100%" height="500px" />
-                ) : (
-                  <p>Loading PDF...</p>
-                )}
-              </div>
-       
+              {pdfUrl ? (
+                <iframe title="PDF Viewer" src={pdfUrl} width="100%" height="500px" />
+              ) : (
+                <p>Loading PDF...</p>
+              )}
+            </div>
 
             {isAcknowledged ? (
-            <strong>    <p className="acknowledge-info">You have already acknowledged this memo.</p></strong>  
-          ) : (
-            <>
-              <label for='acknowledge'>By clicking this button the Admin notify that you acknowledge this memo</label>
-              <button className='acknowledge' onClick={handleAcknowledge}>
-                Acknowledge Memo
-              </button>
-              <p className="acknowledge-info">You haven't acknowledged this memo yet.</p>
-            </>
-          )}
+              <strong><p className="acknowledge-info">You have already acknowledged this memo.</p></strong>
+            ) : (
+              <>
+                <label htmlFor="acknowledge">By clicking this button the Admin will be notified that you acknowledge this memo</label>
+                <button className="acknowledge" onClick={handleAcknowledge}>
+                  Acknowledge Memo
+                </button>
+                <p className="acknowledge-info">You haven't acknowledged this memo yet.</p>
+              </>
+            )}
           </div>
         </div>
       </div>
@@ -189,4 +116,4 @@ if (!memoDetails) {
   );
 };
 
-export default  UserRecieveMemoDetails;
+export default UserRecieveMemoDetails;
