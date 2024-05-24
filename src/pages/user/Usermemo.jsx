3e890cc5668911a2392fb806a1a0d235cd9ed2c2
ALMIKO JASPER L. CABALLES <89283730+Mikoto239@users.noreply.github.com>
2024-05-24 16:56:42 +0800

    Update Usermemo.jsx

--- src/pages/user/Usermemo.jsx
+++ src/pages/user/Usermemo.jsx
@@ -124,15 +124,7 @@ const UserMemoManager = () => {
     <>
     <HeaderDashboard />
     <div className="dashboard">
-      <link 
-        rel="stylesheet" 
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-      />
-      <link 
-        rel="stylesheet" 
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" 
-      />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+
 
       <SidebarUser/>
       
